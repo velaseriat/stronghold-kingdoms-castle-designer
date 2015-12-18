54c85f28314ec509e68103ce4cbbf427f3719470
Kelly Masuda <velaseriat@outlook.com>
2015-12-17 21:13:39 -1000

    Implementation still in progress... finally got a list of all the towers and it's center, which will be necessary in the final step to write to hex file

--- src/main/java/castledesigner/BuildingType.java
+++ src/main/java/castledesigner/BuildingType.java
@@ -25,6 +25,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
@@ -45,41 +46,43 @@ public enum BuildingType
 {
 	// @formatter:off
 	//Wood
-	WOODEN_WALL(new Color(150, 75, 0), new Dimension(1, 1), false, new Integer[] {0, 20, 0, 0}, 225),
-	WOODEN_GATEHOUSE(new Color(100, 50, 0), new Dimension(3, 3), true, new Integer[] {0, 200, 0, 0}, 3600),
-	WOODEN_TOWER(new Color(125, 58, 0), new Dimension(2, 2), false, new Integer[] {0, 200, 0, 0}, 10800),
+	WOODEN_WALL(		new Color(150, 75, 0), 		new Dimension(1, 1), false, 	new Integer[] {0, 20, 0, 0},	225,	"2100",	new Point(0, 0)),
+	WOODEN_GATEHOUSE(	new Color(100, 50, 0), 		new Dimension(3, 3), true, 		new Integer[] {0, 200, 0, 0}, 	3600,	"2700",	new Point(1, 1)),
+	WOODEN_TOWER(		new Color(125, 58, 0), 		new Dimension(2, 2), false, 	new Integer[] {0, 200, 0, 0}, 	10800,	"1500",	new Point(1, 0)),
 	
 	//Stone
-	STONE_WALL(new Color(230, 230, 230), new Dimension(1, 1), false, new Integer[] {100, 0, 0, 0}, 900),
-	STONE_GATEHOUSE(new Color(100, 100, 100), new Dimension(3, 3), true, new Integer[] {500, 0, 0, 0}, 7200),
-	LOOKOUT_TOWER(new Color(200, 200, 200), new Dimension(2, 2), true, new Integer[] {300, 0, 0, 0}, 14400),
-	SMALL_TOWER(new Color(200, 200, 200), new Dimension(3, 3), true, new Integer[] {800, 0, 0, 0}, 28800),
-	LARGE_TOWER(new Color(200, 200, 200), new Dimension(4, 4), true, new Integer[] {1500, 0, 0, 0}, 57600),
-	GREAT_TOWER(new Color(200, 200, 200), new Dimension(5, 5), true, new Integer[] {2500, 0, 0, 0}, 86400),
+	STONE_WALL(			new Color(230, 230, 230), 	new Dimension(1, 1), false, 	new Integer[] {100, 0, 0, 0}, 	900,	"2200",	new Point(0, 0)),
+	STONE_GATEHOUSE(	new Color(100, 100, 100), 	new Dimension(3, 3), true, 		new Integer[] {500, 0, 0, 0}, 	7200,	"2500",	new Point(1, 1)),
+	LOOKOUT_TOWER(		new Color(200, 200, 200), 	new Dimension(2, 2), true, 		new Integer[] {300, 0, 0, 0}, 	14400,	"0b00",	new Point(1, 0)),
+	SMALL_TOWER(		new Color(200, 200, 200), 	new Dimension(3, 3), true, 		new Integer[] {800, 0, 0, 0}, 	28800,	"0c00",	new Point(1, 1)),
+	LARGE_TOWER(		new Color(200, 200, 200), 	new Dimension(4, 4), true, 		new Integer[] {1500, 0, 0, 0}, 	57600,	"0d00",	new Point(2, 1)),
+	GREAT_TOWER(		new Color(200, 200, 200), 	new Dimension(5, 5), true, 		new Integer[] {2500, 0, 0, 0}, 	86400,	"0e00",	new Point(2, 2)),
 
 	//Misc
-	GUARD_HOUSE(new Color(255, 200, 180), new Dimension(3, 3), false, new Integer[] {0, 400, 0, 0}, 10800),
-	BALLISTA_TOWER(new Color(230, 200, 60), new Dimension(3, 3), false, new Integer[] {0, 10000, 0, 0}, 18000),
-	TURRET(new Color(0, 0, 80), new Dimension(2, 2), false, new Integer[] {2000, 0, 0, 0}, 14400),
-	SMELTER(new Color(200, 30, 30), new Dimension(4, 4), false, new Integer[] {0, 0, 400, 0}, 21600),
-	MOAT(new Color(0, 200, 255), new Dimension(1, 1), false, new Integer[] {0, 0, 0, 20}, 900),
-
+	GUARD_HOUSE(		new Color(255, 200, 180), 	new Dimension(3, 3), false, 	new Integer[] {0, 400, 0, 0}, 	10800,	"1f00",	new Point(1, 1)),
+	BALLISTA_TOWER(		new Color(230, 200, 60), 	new Dimension(3, 3), false, 	new Integer[] {0, 10000, 0, 0}, 18000,	"2a00",	new Point(1, 1)),
+	TURRET(				new Color(0, 0, 80), 		new Dimension(2, 2), false, 	new Integer[] {2000, 0, 0, 0}, 	14400,	"2900",	new Point(1, 0)),
+	SMELTER(			new Color(200, 30, 30), 	new Dimension(4, 4), false, 	new Integer[] {0, 0, 400, 0}, 	21600,	"2000",	new Point(2, 1)),
+	MOAT(				new Color(0, 200, 255), 	new Dimension(1, 1), false, 	new Integer[] {0, 0, 0, 20}, 	900,	"2300",	new Point(0, 0)),
+	
 	//Keep
-	KEEP(new Color(0, 0, 0), new Dimension(8, 8), false, new Integer[] {0, 0, 0, 0}, 0),
-	KILLING_PIT(new Color(120, 100, 0), new Dimension(1, 1), false, new Integer[] {0, 0, 100, 0}, 3600),
+	KEEP(				new Color(0, 0, 0), 		new Dimension(8, 8), false, 	new Integer[] {0, 0, 0, 0}, 	0,		"",		new Point(0, 0)),
+	KILLING_PIT(		new Color(120, 100, 0), 	new Dimension(1, 1), false, 	new Integer[] {0, 0, 100, 0}, 	3600,	"2400",	new Point(0, 0)),
 	
 	// additional building in age 4++
-	BOMBARD(new Color(49, 44, 33), new Dimension(3, 3), false, new Integer[]  {0,0,1000, 0}, 43200);
+	BOMBARD(			new Color(49, 44, 33), 		new Dimension(3, 3), false, 	new Integer[] {0,0,1000, 0}, 	43200,	"2c00",	new Point(1, 1));
 	// @formatter:on
 	
-	private final Color colour;
-	private Dimension dimension;
-	private boolean gapRequired;
-	private BufferedImage image;
-	private BufferedImage validOverlay;
-	private BufferedImage invalidOverlay;
-	private Map<BuildingResource, Integer> buildingResources;
-	private int buildTime;
+	private final 	Color 								colour;
+	private 		Dimension 							dimension;
+	private 		boolean 							gapRequired;
+	private 		BufferedImage 						image;
+	private 		BufferedImage 						validOverlay;
+	private 		BufferedImage 						invalidOverlay;
+	private 		Map<BuildingResource, Integer> 		buildingResources;
+	private 		int 								buildTime;
+	private			String								casFileCode;
+	private			Point								casFileOffset;
 	
 	/**
 	 * Constructor
@@ -90,8 +93,21 @@ public enum BuildingType
 	 *            the dimension of the building (in tiles)
 	 * @param gapRequired
 	 *            true if this building cannot be placed next to other buildings that also have this set to true
+	 * @param resourceCosts
+	 * 			  cost of resources
+	 * @param buildTime
+	 * 			  the buildtime of the structure
+	 * @param offset
+	 * 			  the point offset when writing data into .cas files. Unfortunately, it is not from the bottom-left corner, but from the center
 	 */
-	private BuildingType(Color colour, Dimension dimension, boolean gapRequired, Integer[] resourceCosts, int buildTime)
+	private BuildingType(
+			Color 		colour,
+			Dimension 	dimension,
+			boolean 	gapRequired,
+			Integer[] 	resourceCosts,
+			int 		buildTime,
+			String		casFileCode,
+			Point 		casFileOffset)
 	{
 		this.colour = colour;
 		this.dimension = dimension;
@@ -120,6 +136,8 @@ public enum BuildingType
 		buildingResources.put(BuildingResource.GOLD, resourceCosts[3]);
 		
 		this.buildTime = buildTime;
+		this.casFileCode = casFileCode;
+		this.casFileOffset = casFileOffset;
 	}
 	
 	private BufferedImage createOverlay(boolean valid)
@@ -144,10 +162,27 @@ public enum BuildingType
 		return overlay;
 	}
 	
+	
 	public int[] getHotspot()
 	{
 		return new int[]
 		{ (dimension.width - 1) / 2, (dimension.height - 1) / 2 };
+	}
+	
+	//Convenience methods for .cas file output
+	public String getCaseFileCode(){
+		return this.casFileCode;
+	}
+	public Point getCasFilePointOffset(){
+		return this.casFileOffset;
+	}
+	
+	public int xOffset(){
+		return this.casFileOffset.x;
+	}
+	
+	public int yOffset(){
+		return this.casFileOffset.y;
 	}
 	
 	/**
--- src/main/java/castledesigner/CasFileWriter.java
+++ src/main/java/castledesigner/CasFileWriter.java
@@ -3,6 +3,8 @@ package castledesigner;
 import java.awt.Point;
 import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -11,10 +13,25 @@ public class CasFileWriter {
 	TileBuilding[][] gridData;
 	File[] casFiles;
 
-	TreeMap<CastlePoint, BuildingType> castleLayoutMap;
+	private HashMap<CastlePoint, BuildingType> castleLayoutMap;
+	private HashMap<Point, BuildingType> preWriteMap;
 
 	public void writeToCasFiles(){
 		castleLayoutMap = convertGridData(gridData);
+		preWriteMap = addCasFileOffset(castleLayoutMap);
+	}
+
+	private HashMap<Point, BuildingType> addCasFileOffset(HashMap<CastlePoint, BuildingType> clm) {
+		HashMap<Point, BuildingType> output = new HashMap<Point, BuildingType>();
+		
+		for(CastlePoint cp : clm.keySet()){
+			BuildingType bt = clm.get(cp);
+			Point offset = bt.getCasFilePointOffset();
+			Point p = new Point(cp.x + offset.x, cp.y + offset.y);
+			output.put(p, bt);
+		}
+		System.out.println(output);
+		return output;
 	}
 
 	/**
@@ -23,29 +40,34 @@ public class CasFileWriter {
 	 * @param TileBuilding[][] gd
 	 * @return TreeMap<CastlePoint, BuildingType> clm
 	 */
-	private TreeMap<CastlePoint, BuildingType> convertGridData(TileBuilding[][] gd) {
+	private HashMap<CastlePoint, BuildingType> convertGridData(TileBuilding[][] gd) {
 		//HashMap<BuildingId, TreeSet<CastlePoint>> to filter same building IDs
 		HashMap<Integer, TreeSet<CastlePoint>> structureMap = new HashMap<Integer, TreeSet<CastlePoint>>();
 		for (int i = 0; i < gridData.length; i++)
 			for (int j = 0; j < gridData[i].length; j++){
 				//System.out.println("("+ i + "," + j + ")" + (gridData[i][j] != null ? gridData[i][j].getBuildingType().name() + " ID:" + gridData[i][j].getBuildingId() : "Empty spot"));
 				if (gridData[i][j] != null)
-					if (!structureMap.containsKey(gridData[i][j].getBuildingId()))
-						structureMap.put(gridData[i][j].getBuildingId(), new TreeSet<CastlePoint>());
-					else
+					if (gridData[i][j].getBuildingType() != BuildingType.KEEP){
+						
+						if (!structureMap.containsKey(gridData[i][j].getBuildingId())){
+							structureMap.put(gridData[i][j].getBuildingId(), new TreeSet<CastlePoint>());
+							//System.out.println("("+ i + "," + j + ")" + (gridData[i][j] != null ? gridData[i][j].getBuildingType().name() + " ID:" + gridData[i][j].getBuildingId() : "Empty spot"));
+						}
 						structureMap.get(gridData[i][j].getBuildingId()).add(new CastlePoint(i, j));
+					}
 			}
-		TreeMap<CastlePoint, BuildingType> clm = new TreeMap<CastlePoint, BuildingType>();
-		for (int i : structureMap.keySet()){
+		
+		//take the top of each list so that we can get the bottom-right corner of each structure.
+		HashMap<CastlePoint, BuildingType> clm = new HashMap<CastlePoint, BuildingType>();
+		for (Integer i : structureMap.keySet()){
 			//simply take the first value in each entry
 			clm.put(structureMap.get(i).first(), getBuildingId(structureMap.get(i).first()));
 		}
-
 		return clm;
 	}
 
 	private BuildingType getBuildingId(CastlePoint point) {
-		return gridData[(int) point.getX()][(int) point.getY()].getBuildingType();
+		return gridData[point.x][point.y].getBuildingType();
 	}
 
 	public TileBuilding[][] getGridData() {
@@ -62,19 +84,17 @@ public class CasFileWriter {
 	}
 
 	//for the sake of getting the correct point in the TreeSet. Not good programming but it gets it done.
-	class CastlePoint extends Point implements Comparable{
+	class CastlePoint extends Point implements Comparable<CastlePoint>{
 		public CastlePoint(int x, int y){
 			super (x, y);
 		}
-
-		public int compareTo(Object other) {
-			if (other instanceof Point){
-				if (((Point) other).getX() + ((Point) other).getY() < this.getX() + this.getY()){
-					return 1;
-				}
-				else if (((Point) other).getX() + ((Point) other).getY() > this.getX() + this.getY()){
-					return -1;
-				}
+		
+		public int compareTo(CastlePoint other) {
+			if (other.x + other.y < this.x + this.y){
+				return 1;
+			}
+			else if (other.x + other.y > this.x + this.y){
+				return -1;
 			}
 			return 0;
 		}
